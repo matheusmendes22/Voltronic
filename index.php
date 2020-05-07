6cd28af10e342776387171ebaf2b7df2f47dd2fa
Matheus Mendes <matheus.mendes2298@gmail.com>
2020-05-07 17:56:33 -0300

    Iniciado na home bloco de contato, a finalizar

--- index.php
+++ index.php
@@ -33,20 +33,21 @@
       </a>
     </div>
     <!-- Sobre -->
-    <section class="about-area pt-5 pb-4">
+    <section class="about-area pt-5 pb-5">
         <div class="container">
         <div class="row align-items-lg-center">
           <div class="col-md-6 mb-5 mb-lg-0 position-relative">
             <img src="img/about/about_1.jpg" class="img-fluid" alt="Image">
             <div class="experience">
-              <span class="year">50 anos</span>
+              <span class="year">25 anos</span>
               <span class="caption">de experiência</span>
             </div>
           </div>
           <div class="col-md-5 ml-auto">
-            <h3 class="section-sub-title">Sobre Nós</h3>
-            <h2 class="section-title mb-3">Bem-vindo à Medcare</h2>
-            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui fuga ipsa, repellat blanditiis nihil, consectetur. Consequuntur eum inventore, rem maxime, nisi excepturi ipsam libero ratione adipisci alias eius vero vel!</p>
+            <h3 class="section-sub-title">Sobre a Voltronic Power solution</h3>
+            <h2 class="section-title mb-3">Oferecendo energia ininterrupta para você cliente!</h2>
+            <p class="mb-4">Voltronic Power solution Assistência, Comércio e Serviços em Eletrônica Ltda.
+            Há mais de 25 anos oferendo energia ininterrupta a seus clientes dentro de todo o mercado nacional. </p>
             <p><a href="sobre-nos.php" class="btn main_btn">Sobre Nós</a></p>
           </div>
         </div>
@@ -57,7 +58,7 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-12 ml-auto">
-            <div class="row mb-4">
+            <div class="row mb-4 d-flex justify-content-center">
               <div class="col-12 text-center mb-4">
                 <h3 class="section-sub-title">Serviços</h3>
                 <h2 class="section-title mb-4">Conheça os serviços oferecidos pela Voltronic</h2>
@@ -67,30 +68,42 @@
                             <img class="img-fluid w-100" src="img/produtos/baterias.jpg" alt="">
                   <h3 class="text-center mt-3">Comércio de No-Breaks e Estabilizadores</h3>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nostrum ducimus temporibus, quod ipsa fuga fugiat deserunt.</p>
-                  <p><a href="#" class="readmore">Learn more</a></p>
+                  <p><a href="#" class="readmore">Saiba Mais</a></p>
                 </div>
               </div>
               <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
                 <div class="service h-100">
                             <img class="img-fluid w-100" src="img/produtos/no-break.jpg" alt="">
-                  <h3 class="text-center mt-3">Web Development</h3>
+                  <h3 class="text-center mt-3">Manutenções preventivas e corretivas</h3>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nostrum ducimus temporibus, quod ipsa fuga fugiat deserunt.</p>
-                  <p><a href="#" class="readmore">Learn more</a></p>
+                  <p><a href="#" class="readmore">Saiba mais</a></p>
                 </div>
               </div>
               <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
                 <div class="service h-100">
                   <img src="img/produtos/20190628184154.jpg" alt="">
-                  <h3 class="text-center mt-3">Inversores solares</h3>
+                  <h3 class="text-center mt-3">Instalação de equipamentos</h3>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nostrum ducimus temporibus, quod ipsa fuga fugiat deserunt.</p>
-                  <p><a href="#" class="readmore">Learn more</a></p>
+                  <p><a href="#" class="readmore">Saiba mais</a></p>
                 </div>
               </div>
-              <div class="col-md-12 d-flex justify-content-center">
-                <a class="btn main_btn" href="produtos">
-                  Conheça todos nossos produtos
-                </a>
+              <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+                <div class="service h-100">
+                  <img src="img/produtos/20190628184154.jpg" alt="">
+                  <h3 class="text-center mt-3">Elétrica em geral</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nostrum ducimus temporibus, quod ipsa fuga fugiat deserunt.</p>
+                  <p><a href="#" class="readmore">Saiba mais</a></p>
+                </div>
               </div>
+              <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+                <div class="service h-100">
+                  <img src="img/produtos/20190628184154.jpg" alt="">
+                  <h3 class="text-center mt-3">Cabeamento estruturado</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nostrum ducimus temporibus, quod ipsa fuga fugiat deserunt.</p>
+                  <p><a href="#" class="readmore">Saiba mais</a></p>
+                </div>
+              </div>
+             
              
             </div>
           </div>
@@ -98,7 +111,7 @@
       </div>
     </div>
     <!-- CTA -->
-    <section class="hotline-area text-center area-padding">
+    <section class="hotline-area text-center py-4">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
@@ -153,6 +166,44 @@
           </div>
         </div>
       </div>
+    </div>
+    <!-- location-area-start -->
+    <div class="addres-area black-bg section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-4 col-md-4">
+                    <div class="single-address text-center">
+                        <div class="addres-icon">
+                            <img src="img/icon/1.png" alt="Localização" title="localizaçao">
+                        </div>
+                        <h3>Onde estamos</h3>
+                        <p>Rua Lima Campos, 118.<br>
+                         Jd. Nordeste. São Paulo –SP</p>
+                        <a class="underline-hover" target="_blank" href="https://www.google.com/maps?client=firefox-b-d&q=Rua+Lima+Campos,+118.Jd.+Nordeste.+S%C3%A3o+Paulo+%E2%80%93SP&um=1&ie=UTF-8&sa=X&ved=2ahUKEwiFz63H9anoAhV-JLkGHVhgB2gQ_AUoAXoECAwQAw">Venha nos visitar</a>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4">
+                    <div class="single-address text-center">
+                        <div class="addres-icon">
+                            <img src="img/icon/2.png" alt="">
+                        </div>
+                        <h3>Horários de Funcionamento</h3>
+                        <p>Seg-Sex (8:00 às 19:00) <br></p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4">
+                    <div class="single-address text-center">
+                        <div class="addres-icon">
+                            <img src="img/icon/4.png" alt="">
+                        </div>
+                        <h3>Atendimento via WhatsApp</h3>
+                        <p>(11) 98285-6401</p>
+                        <a class="underline-hover" target="_blank" href="https://wa.me/5511982856401">Clique Aqui</a>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <!-- Footer -->
     <? include 'inc/inc.footer.php'; ?>
